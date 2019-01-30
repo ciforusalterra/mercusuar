50b1b9684e6efeca0ed06807f26bb20776b64cfc
Christoforus Surjoputro <christoforus@alterra.id>
2019-01-30 19:17:45 +0700

    Fix model problem for serialization to json

--- notifier/model.py
+++ notifier/model.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 
 class Log:
-    id: int = -1
-    status: str = ''
-    message: str = ''
-    created_date: datetime = datetime.now()
+
+    def __init__(self):
+        self.id: int = -1
+        self.status: str = ''
+        self.message: str = ''
+        self.created_date: datetime = datetime.now()
